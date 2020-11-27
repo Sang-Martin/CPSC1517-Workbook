818288bb3df9615835ecb44688e9f916694108ce
Sang-Martin <59576275+Sang-Martin@users.noreply.github.com>
2020-11-26 18:49:18 -0700

    GridViewCodeBehind

---
+++ CSNet/NorthWindSystem/BLL/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Aditional Namespaces
+using NorthwindSystem.Entities;
+using NorthwindSystem.DAL;
+using System.Data.SqlClient;
+#endregion
+
+namespace NorthwindSystem.BLL
+{
+    public class ProductController
+    {
+        //lookup by primary key
+        public Product Product_FindByID(int productid)
+        {
+            using(var context = new NorthwindSystemContext())
+            {
+                return context.Products.Find(productid);
+            }
+        }
+
+        //lookup using an non primary key field
+        public List<Product> Product_GetByPartialProductName (string proudctname)
+        {
+            using(var context = new NorthwindSystemContext())
+            {
+                IEnumerable<Product> results = context.Database.SqlQuery<Product>("Products_GetByPartialProductName @PartialName",
+                    new SqlParameter("PartialName", proudctname));
+                return results.ToList();
+            }
+        }
+    }
+}
